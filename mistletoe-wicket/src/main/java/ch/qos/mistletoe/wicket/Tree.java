bda9bbf785084737ce674a3002b008e22fd66983
Ceki Gulcu <ceki@qos.ch>
2009-10-09 22:46:27 +0200

    - minor clean up

--- mistletoe-wicket/src/main/java/ch/qos/mistletoe/wicket/Tree.java
+++ mistletoe-wicket/src/main/java/ch/qos/mistletoe/wicket/Tree.java
@@ -13,9 +13,17 @@ public class Tree extends WebPage {
     MistletoeCore mCore = new MistletoeCore(MyCollection.class);
     mCore.run();
     
-    Description description = mCore.getDescription();
-    
-    DescriptionPanel nodePanel = new DescriptionPanel("node", description, mCore);
+    Description rootDescription = mCore.getDescription();
+    rootDescription = getFistChildIfNecessary(rootDescription);
+    DescriptionPanel nodePanel = new DescriptionPanel("node", rootDescription, mCore);
     add(nodePanel);
+  }
+  
+  Description getFistChildIfNecessary(Description description) {
+    if(description.getDisplayName().equals("null")) {
+      return description.getChildren().get(0);
+    } else {
+      return description;
+    }
   }
 }
