e254e30522f269473613524b43e6cd3e90ec7356
Ceki Gulcu <ceki@qos.ch>
2010-01-21 11:39:44 +0100

    - cosmetic changes, css files copies over from cal10n
    - in pom.xml references to pixie changed to pixie.qos.ch

--- mistletoe-site/src/site/pages/templates/left.js
+++ mistletoe-site/src/site/pages/templates/left.js
@@ -1,18 +1,24 @@
 
+document.write('<div class="menuGroup">');
 document.write('<p class="menu_header">Mistletoe project</p>');
-document.write('<p class="menu"><a href="' + prefix + 'index.html">Introduction</a></p>');
-document.write('<p class="menu"><a href="' + prefix + 'news.html">News</a></p>');
-document.write('<p class="menu"><a href="' + prefix + 'download.html">Download</a></p>');
-document.write('<p class="menu"><a href="' + prefix + 'documentation.html">Documentation</a></p>');
-document.write('<p class="menu"><a href="' + prefix + 'mailinglist.html">Mailing Lists</a></p>');
-document.write('<p class="menu"><a href="' + prefix + 'repos.html">Source Repository</a></p>');
-document.write('<p class="menu"><a href="' + prefix + 'bugreport.html">Bug Report</a></p>');
-document.write('<p class="menu"><a href="' + prefix + 'license.html">License</a></p>');
+document.write('  <a href="' + prefix + 'index.html">Introduction</a>');
+document.write('  <a href="' + prefix + 'news.html">News</a>');
+document.write('  <a href="' + prefix + 'download.html">Download</a>');
+document.write('  <a href="' + prefix + 'documentation.html">Documentation</a>');
+document.write('  <a href="' + prefix + 'mailinglist.html">Mailing Lists</a>');
+document.write('  <a href="' + prefix + 'repos.html">Source Repository</a>');
+document.write('  <a href="' + prefix + 'bugreport.html">Bug Report</a>');
+document.write('  <a href="' + prefix + 'license.html">License</a>');
+document.write('</div>');
 
-document.write('<p><a class="x" href="http://www.qos.ch/"><img src="' + prefix + 'images/logos/qosLogo.png" /></a></p>');
-
-
+document.write('<p>&nbsp;</p>');
+document.write('<p class="pub">');
+document.write('  <a href="http://www.qos.ch/shop/products/log4jManual">');
+document.write('    <img src="images/buyDirect.jpg" border="0" title="" alt="buy direct from the developer"/>');
+document.write('  </a>');
 document.write('</p>');
+
+
 
 
 
